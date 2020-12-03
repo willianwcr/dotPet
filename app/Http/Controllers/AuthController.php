707f51203f3a2d05a985ba326a347a6c249ff060
Willian Rodrigues <willian@willianwcr.com.br>
2020-12-03 00:58:45 -0300

    Logout redirect page and seeder

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,6 +31,6 @@ class AuthController extends Controller
 
     public function logout(){
         Auth::logout();
-        return redirect()->route('home');
+        return redirect()->route('login.showForm');
     }
 }
