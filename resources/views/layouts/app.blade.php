dee1e5730d2925d8df4a8c7c5e946250ed45f6c0
Willian Rodrigues <willian@willianwcr.com.br>
2020-12-04 08:10:31 -0300

    Sass installed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="theme-color" content="#EF476F" />
     <title>@yield('title') - dotpet.</title>
     <link rel="icon" href="{{ asset('/img/icon.png') }}">
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/ac7344cbc2.js" crossorigin="anonymous"></script>
 </head>
