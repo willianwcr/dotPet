507a2b7e44d0946bf690b7d8d8bd216a36488d9b
Willian Rodrigues <willian@willianwcr.com.br>
2020-12-09 14:14:33 -0300

    Home elements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,18 @@
 @section('title', 'Início')
 
 @section('content')
-    Home
+    <div class="home row">
+        <div class="herobanner col-7">
+            <h1>Adotar é amar em dobro 💗🐾</h1>
+            <div class="form-input form-input-icon input-secondary">
+                <div class="form-icon">
+                    <i class="fas fa-search"></i>
+                </div>
+                <input type="email" class="form-input" name="email" placeholder="Pesquise por bairro, raça, porte, etc.">
+                <button class="btn btn-primary"><i class="fas fa-chevron-right"></i></button>
+            </div>
+        </div>
+        <div class="image col-5" style="background-image: url({{ asset('./img/pulgas.jpg') }})">
+        </div>
+    </div>
 @endsection
